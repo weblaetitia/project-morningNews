25da085729e6899388b3b551e131df4f1d224ebb
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-21 10:46:22 +0200

    set-up 'signup' route in back-end

---
+++ models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Schema
+var userSchema = mongoose.Schema({
+    name: String,
+    email : String,
+    password: String
+})
+
+// model
+var UsersModel = mongoose.model('users',userSchema)
+
+// export models 
+module.exports = UsersModel
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const UsersModel = require('../models/Users');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/*      SIGN_UP     */
+/*   Add new user   */
+router.post('/sign-up', async function(req, res, next) {
+  var newUser = new UsersModel({
+    name: req.params.name,
+    email: req.params.name,
+    password: req.params.password,
+  })
+  var newUser = await newUser.save()
+})
 
 module.exports = router;
