34850bfbb3eb0b697825dd93879a2a9d4e6bb07e
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-21 12:18:06 +0200

    set 'sign-up' functionality

--- reactapp/src/ScreenHome.js
+++ reactapp/src/ScreenHome.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Input, Button } from "antd";
+import {Redirect} from 'react-router-dom'
 
 function ScreenHome() {
+
+  // faire des setter pour name, email, passwors
+  // faire des inputs
+  // au clic -> passer les infos à la bdd avec fetch
+
+
+  const[signUpName, setSignUpName] = useState()
+  const[signUpEmail, setSignUpEmail] = useState()
+  const[signUpPassword, setSignUpPassword] = useState()
+
+  const[redirect, setRedirect] = useState()
+
+  var handleSubmitSignUp = async () => {
+    var rawResponse = await fetch('/sign-up', {
+      method: 'POST',
+      headers: {'Content-Type':'application/x-www-form-urlencoded'},
+      body: 'name=' + signUpName + '&email='+ signUpEmail + '&password=' + signUpPassword
+    })
+    if (rawResponse)  {
+      setRedirect('/sources')
+    }
+  }
+  
+
   return (
     <div className="Login-page">
+      <Redirect to={redirect} />
+      
       {/* SIGN-IN */}
 
       <div className="Sign">
-        <Input className="Login-input" placeholder="arthur@lacapsule.com" />
-        <Input.Password className="Login-input" placeholder="password" />
-        <Button href="" style={{ width: "80px" }} type="primary">
+        <Input 
+        className="Login-input" 
+        placeholder="arthur@lacapsule.com" 
+        // onChange={ (e) => setUserEmail(e.target.value)}
+        // value={userEmail}
+        />
+        <Input.Password 
+        className="Login-input" 
+        placeholder="password" 
+        // onChange={ (e) => setUserPassword(e.target.value)}
+        // value={userPassword}
+        />
+        <Button style={{ width: "80px" }} type="primary">
           Sign-in
         </Button>
       </div>
@@ -18,10 +55,26 @@ function ScreenHome() {
       {/* SIGN-UP */}
 
       <div className="Sign">
-        <Input className="Login-input" placeholder="Arthur G" />
-        <Input.Password className="Login-input" placeholder="password" />
-        <Button href="" style={{ width: "80px" }} type="primary">
-          Sign-up
+      <Input 
+        className="Login-input" 
+        placeholder="arthur93" 
+        onChange={ (e) => setSignUpName(e.target.value)}
+        value={signUpName}
+        />
+        <Input 
+        className="Login-input" 
+        placeholder="arthur@lacapsule.com" 
+        onChange={ (e) => setSignUpEmail(e.target.value)}
+        value={signUpEmail}
+        />
+        <Input.Password 
+        className="Login-input" 
+        placeholder="password" 
+        onChange={ (e) => setSignUpPassword(e.target.value)}
+        value={signUpPassword}
+        />
+        <Button style={{ width: "80px" }} type="primary" onClick={ () => handleSubmitSignUp()}>
+          Sign-in
         </Button>
       </div>
     </div>
