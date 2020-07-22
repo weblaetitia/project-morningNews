da7660025a70e80f015d6ab48bead3555b345375
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-22 16:46:31 +0200

    add a condition to wish-list to avoid double articles

--- reactapp/src/reducers/article.reducer.js
+++ reactapp/src/reducers/article.reducer.js
@@ -2,9 +2,25 @@
 
 export default function(wishList = [], action) {
   if(action.type == 'addArticle') {
-    var newArray = [...wishList];
-    newArray.push({wlTitle: action.wlTitle, wlContent: action.wlContent, wlImage: action.wlImage})
-    return newArray;
+
+    var find = false 
+    
+    wishList.forEach(element => {
+      if (element.wlTitle == action.wlTitle) {
+        find = true
+      }
+    })
+
+
+    if (!find) {
+      var newArray = [...wishList];
+      newArray.push({wlTitle: action.wlTitle, wlContent: action.wlContent, wlImage: action.wlImage})
+      return newArray;
+    } else {
+      return wishList
+    }
+
+    
   } else if (action.type == 'delete') {
     var filterArray = wishList.filter(element => element.wlTitle !== action.wlTitle)
     return filterArray;
