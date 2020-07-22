ed2972bebfeda271c52ad1b93ed8972eb2cbe533
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-22 15:23:52 +0200

    use redux to get articles from wishlist store

--- reactapp/src/ScreenMyArticles.js
+++ reactapp/src/ScreenMyArticles.js
@@ -11,16 +11,7 @@ import {connect} from 'react-redux';
 const { Meta } = Card;
 
 function ScreenMyArticles(props) {
-  const FakeArticles = [
-    {
-      title: "Bitcoin Power",
-      description: "Le bitcoin revient de très loin et peut toujours...",
-      img: "/images/bitcoin.jpg",
-      content:
-        "L’agenda politique sur la monnaie numérique publique, ainsi que ma récente visite du Musée créé par la Banque de France m’ont conduit à de multiples réflexions. Qu’un ministre ne veuille pas de monnaie privée sur notre sol (curieux, d’ailleurs que ce soit précisément ce mot à double sens qui affleure ici) c’est une chose. Que la chose soit impensable en est une autre.On va parler ici d’une monnaie privée émise justement par… un ministre, et pas n’importe lequel. Au cœur de l’appareil d’Etat, et en plein centre de la France. ",
-    }
-  ];
-
+  
   const articles = props.myArticles.map( (article) => {
     return  <Col span={8} >
               <Card style={{marginTop:'2em'}}
